55ce54f8b22e142f496d92725756317a1356bad6
John Gannaway <johngannaway1@yahoo.co.uk>
2020-08-08 20:39:40 +0200

    Small security update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,5 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyBfjKpLKaEB3zxTfCQncdTIVlvjoYXMhqA",
-    authDomain: "commanders-console.firebaseapp.com",
-    databaseURL: "https://commanders-console.firebaseio.com",
-    projectId: "commanders-console",
-    storageBucket: "commanders-console.appspot.com",
-    messagingSenderId: "1041761144583",
-    appId: "1:1041761144583:web:261c32ff2eccedd9a2ffa2",
-    measurementId: "G-T552HEQSKZ"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,6 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBfjKpLKaEB3zxTfCQncdTIVlvjoYXMhqA",
-    authDomain: "commanders-console.firebaseapp.com",
-    databaseURL: "https://commanders-console.firebaseio.com",
-    projectId: "commanders-console",
-    storageBucket: "commanders-console.appspot.com",
-    messagingSenderId: "1041761144583",
-    appId: "1:1041761144583:web:261c32ff2eccedd9a2ffa2",
-    measurementId: "G-T552HEQSKZ"
   }
 };
 
